78ea2243c11fe22c8cd16a79d2c96e5f3eb5ac76
jordanschuetz <jordan@ninjapigstudios.com>
2017-04-28 15:26:16 -0700

    eslint complete

--- js/playState.js
+++ js/playState.js
@@ -12,35 +12,30 @@ function logCurrentStateCoin(game, coin) {
   for (value of window.globalLevelState.coinCache.coins) {
     if (coin.x === value.x) {
       window.globalLevelState.coinCache.coins.splice(window.globalLevelState.coinCache.coins.indexOf(value), 1);
-        // console.log(value)
+      // console.log(value)
     }
   }
   fireCoins();
-    // console.log(window.globalLevelState.coinCache.coins)
+  // console.log(window.globalLevelState.coinCache.coins)
 }
 
 window.PlayState = {
-
   init(data) {
     this.keys = this.game.input.keyboard.addKeys({
       left: Phaser.KeyCode.LEFT,
       right: Phaser.KeyCode.RIGHT,
       up: Phaser.KeyCode.UP
     });
-
-
     this.coinPickupCount = 0;
     keyCollected = false;
     this.level = (data.level || 0);
   },
-
   create() {
     window.globalGameState = this;
-        // console.log('window.globalGameState created' , this.level);
-        // fade in  (from black)
+    // console.log('window.globalGameState created' , this.level);
+    // fade in  (from black)
     this.camera.flash('#000000');
-
-        // create sound entities
+    // create sound entities
     this.sfx = {
       jump: this.game.add.audio('sfx:jump'),
       coin: this.game.add.audio('sfx:coin'),
@@ -48,13 +43,12 @@ window.PlayState = {
       stomp: this.game.add.audio('sfx:stomp'),
       door: this.game.add.audio('sfx:door')
     };
-
-        // create level entities and decoration
+    // create level entities and decoration
     this.game.add.image(0, 0, 'background');
     window.textObject1 = this.game.add.text(700, 5, window.text1, { font: 'Bold 200px Arial', fill: '#000000', fontSize: '20px' });
     window.textObject2 = this.game.add.text(700, 35, window.text2, { font: 'Bold 200px Arial', fill: '#000000', fontSize: '20px' });
     window.textObject3 = this.game.add.text(700, 65, window.text3, { font: 'Bold 200px Arial', fill: '#000000', fontSize: '20px' });
-    console.log(window.text);
+    // console.log(window.text);
     if (window.globalLevelState === null) {
       window.globalLevelState = {
         time: 0,
@@ -62,8 +56,8 @@ window.PlayState = {
       };
     }
     this._loadLevel(window.globalLevelState.coinCache);
-        // this._loadLevel(window.globalLevelState.value);
-        // create UI score boards
+    // this._loadLevel(window.globalLevelState.value);
+    // create UI score boards
     this._createHud();
   },
 
@@ -80,7 +74,7 @@ window.PlayState = {
         // this.bgm.stop();
   },
 
-  _canHeroEnterDoor(hero, door) {
+  _canHeroEnterDoor(hero) {
     return keyCollected && hero.body.touching.down;
   },
 
@@ -93,42 +87,39 @@ window.PlayState = {
         let collidePlayer = this.game.physics.arcade.collide(otherplayer, this.hero, null, null, this);
         if (this.hero.y > 526) {
           this.hero.position.set(this.hero.x, 525);
-          console.log('set');
+          // console.log('set');
         } else if (otherplayer > 526) {
           this.otherplayer.position.set(otherplayer.x, 525);
-          console.log('set2');
+          // console.log('set2');
         }
         this.game.physics.arcade.collide(otherplayer, this.platforms, null, null, this);
         this.game.physics.arcade.overlap(otherplayer, this.coins, this._onHeroVsCoin, null, this);
         this.game.physics.arcade.overlap(otherplayer, this.key, this._onHeroVsKey, null, this);
         this.game.physics.arcade.overlap(otherplayer, this.door, this._onOtherHeroVsDoor, this._canHeroEnterDoor, this);
-                    //  send me message that you died FIXME
       }
-
-            // hero vs coins (pick up)
+      // hero vs coins (pick up)
       this.game.physics.arcade.overlap(this.hero, this.coins, this._onHeroVsCoin, null, this);
-            // hero vs key (pick up)
+      // hero vs key (pick up)
       this.game.physics.arcade.overlap(this.hero, this.key, this._onHeroVsKey, null, this);
-            // hero vs door (end level)
+      // hero vs door (end level)
       this.game.physics.arcade.overlap(this.hero, this.door, this._onHeroVsDoor, this._canHeroEnterDoor, this);
-            // ignore if there is no key or the player is on air
+      // ignore if there is no key or the player is on air
     }
   },
 
   _handleInput() {
     window.handleKeyMessages();
-
     //  logCurrentState(this.game);
     if (this.hero) { // Added this so we can control spawning of heros
       if (this.keys.left.isDown) {
         if (!keyStates.leftIsDown) {
-                    // console.log('left pushed');
+          // console.log('left pushed');
           sendKeyMessage({ left: 'down' });
         }
         keyStates.leftIsDown = true;
       } else {
         if (keyStates.leftIsDown) {
-                    // console.log('left un-pushed');
+          // console.log('left un-pushed');
           sendKeyMessage({ left: 'up' });
         }
         keyStates.leftIsDown = false;
@@ -136,13 +127,13 @@ window.PlayState = {
 
       if (this.keys.right.isDown) {
         if (!keyStates.rightIsDown) {
-                    // console.log('right pushed');
+          // console.log('right pushed');
           sendKeyMessage({ right: 'down' });
         }
         keyStates.rightIsDown = true;
       } else {
         if (keyStates.rightIsDown) {
-                    // console.log('right un-pushed');
+          // console.log('right un-pushed');
           sendKeyMessage({ right: 'up' });
         }
         keyStates.rightIsDown = false;
@@ -157,7 +148,7 @@ window.PlayState = {
           keyStates.upIsDown = true;
         } else {
           if (keyStates.upIsDown) {
-                        // console.log('up un-pushed');
+            // console.log('up un-pushed');
             sendKeyMessage({ up: 'up' });
           }
           keyStates.upIsDown = false;
@@ -172,19 +163,19 @@ window.PlayState = {
         this.hero.move(0);
       }
 
-            // handle jump
+      // handle jump
       let jumped = false;
       const JUMP_HOLD = 10;// 200; // ms
       if (this.keys.up.downDuration(JUMP_HOLD)) {
-                // let didJump = this.hero.jump();
-                // if (didJump) { this.sfx.jump.play();}
+        // let didJump = this.hero.jump();
+        // if (didJump) { this.sfx.jump.play();}
       }
 
 
       for (let uuid of globalOtherHeros.keys()) {
         let otherplayer = globalOtherHeros.get(uuid);
         if (Date.now() + JUMP_HOLD <= otherplayer.jumpStart) {
-                    // otherplayer.jump();
+          // otherplayer.jump();
         }
         if (otherplayer.goingLeft) { // move hero left
           otherplayer.move(-1);
@@ -198,7 +189,7 @@ window.PlayState = {
   },
 
   _onHeroVsKey(hero, key) {
-        // this.sfx.key.play();
+    // this.sfx.key.play();
     this.door.frame = 1;
     key.kill();
     keyCollected = true;
@@ -206,22 +197,21 @@ window.PlayState = {
   },
 
   _onHeroVsCoin(hero, coin) {
-        // this.sfx.coin.play();
+    // this.sfx.coin.play();
     coin.kill();
     logCurrentStateCoin(this.game, coin);
     this.coinPickupCount++;
   },
 
   _onHeroVsDoor(hero, door) {
-        // 'open' the door by changing its graphic and playing a sfx
+      // 'open' the door by changing its graphic and playing a sfx
     door.frame = 1;
-        // this.sfx.door.play();
-
-        // play 'enter door' animation and change to the next level when it ends
+      // this.sfx.door.play();
+      // play 'enter door' animation and change to the next level when it ends
     hero.freeze();
     this.game.add.tween(hero)
-            .to({ x: this.door.x, alpha: 0 }, 0, null, true)
-            .onComplete.addOnce(this._goToNextLevel, this);
+      .to({ x: this.door.x, alpha: 0 }, 0, null, true)
+      .onComplete.addOnce(this._goToNextLevel, this);
   },
 
   _onOtherHeroVsDoor(hero, door) {
@@ -287,9 +277,9 @@ window.PlayState = {
   },
 
   _addOtherCharacter(uuid) {
-    console.log('Added another character to game');
+    // console.log('Added another character to game');
     if (globalOtherHeros.has(uuid)) { return; }
-        // console.log('_addOtherCharacter', uuid);
+    // console.log('_addOtherCharacter', uuid);
     this.hero2 = new Hero(this.game, 10, 10);
     this.game.add.existing(this.hero2);
     globalOtherHeros.set(uuid, this.hero2);
@@ -307,36 +297,30 @@ window.PlayState = {
     playerText = this.game.add.text(this.hero.position.x - 10, this.hero.position.y - 550, 'me', { fill: '#000000', fontSize: '15px' });
     playerText.anchor.set(0.5);
     this.hero.addChild(playerText);
-    console.log(playerText.position.x, playerText.position.y);
+    // console.log(playerText.position.x, playerText.position.y);
     window.globalMyHero = this.hero;
     window.globalOtherHeros = this.otherHeros = new Map();
     this.game.add.existing(this.hero);
-
-            // globalMyHero.alpha = 1; //compensating for lag
+    // globalMyHero.alpha = 1; //compensating for lag
     sendKeyMessage({});
   },
 
   _spawnPlatform(platform) {
-    let sprite = this.platforms.create(
-            platform.x, platform.y, platform.image);
-
-        // physics for platform sprites
+    let sprite = this.platforms.create(platform.x, platform.y, platform.image);
+    // physics for platform sprites
     this.game.physics.enable(sprite);
     sprite.body.allowGravity = false;
     sprite.body.immovable = true;
-        // console.log("dank", sprite.body.overlapY)
-    // this.game.debug.body(sprite);
+    // console.log("dank", sprite.body.overlapY)
   },
 
   _spawnCoin(coin) {
     let sprite = this.coins.create(coin.x, coin.y, 'coin');
     sprite.anchor.set(0.5, 0.5);
-
-        // physics (so we can detect overlap with the hero)
+    // physics (so we can detect overlap with the hero)
     this.game.physics.enable(sprite);
     sprite.body.allowGravity = false;
-
-        // animations
+    // animations
     sprite.animations.add('rotate', [0, 1, 2, 1], 6, true); // 6fps, looped
     sprite.animations.play('rotate');
   },
@@ -344,17 +328,16 @@ window.PlayState = {
   _spawnKey(x, y) {
     this.key = this.bgDecoration.create(x, y, 'key');
     this.key.anchor.set(0.5, 0.5);
-        // enable physics to detect collisions, so the hero can pick the key up
+    // enable physics to detect collisions, so the hero can pick the key up
     this.game.physics.enable(this.key);
     this.key.body.allowGravity = false;
-
-        // add a small 'up & down' animation via a tween
+    // add a small 'up & down' animation via a tween
     this.key.y -= 3;
     this.game.add.tween(this.key)
-            .to({ y: this.key.y + 6 }, 800, Phaser.Easing.Sinusoidal.InOut)
-            .yoyo(true)
-            .loop()
-            .start();
+      .to({ y: this.key.y + 6 }, 800, Phaser.Easing.Sinusoidal.InOut)
+      .yoyo(true)
+      .loop()
+      .start();
   },
 
   _spawnDoor(x, y) {
@@ -366,15 +349,13 @@ window.PlayState = {
 
   _createHud() {
     const NUMBERS_STR = '0123456789X ';
-    this.coinFont = this.game.add.retroFont('font:numbers', 20, 26,
-            NUMBERS_STR, 6);
+    this.coinFont = this.game.add.retroFont('font:numbers', 20, 26, NUMBERS_STR, 6);
 
     this.keyIcon = this.game.make.image(0, 19, 'icon:key');
     this.keyIcon.anchor.set(0, 0.5);
 
     let coinIcon = this.game.make.image(this.keyIcon.width + 7, 0, 'icon:coin');
-    let coinScoreImg = this.game.make.image(coinIcon.x + coinIcon.width,
-            coinIcon.height / 2, this.coinFont);
+    let coinScoreImg = this.game.make.image(coinIcon.x + coinIcon.width, coinIcon.height / 2, this.coinFont);
     coinScoreImg.anchor.set(0, 0.5);
 
     this.hud = this.game.add.group();
@@ -383,5 +364,4 @@ window.PlayState = {
     this.hud.add(this.keyIcon);
     this.hud.position.set(10, 10);
   }
-
 };
